6c0925b64ddb5d7eef128007daa86a8f2927a610
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-09-24 10:38:55 -0700

    multi s eletc search using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import Pill from './components/Pill';
 
-function App() {
+// https://dummyjson.com/users/search?q=${searchTerm}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [selectedUserSet, setSelectedUserSet] = useState(new Set());
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const fetchUsers = () => {
+      if(searchTerm.trim() === '') {
+        setSuggestions([]);
+        return;
+      }
+      fetch(`https://dummyjson.com/users/search?q=${searchTerm}`)
+      .then((response) => response.json())
+      .then((data) => setSuggestions(data))
+      .catch((error) => console.log(error))
+    };
+    return fetchUsers();
+  }, [searchTerm]);
+
+  const handleSelectUser = (user) => {
+    setSelectedUsers([...selectedUsers, user]);
+    setSelectedUserSet(new Set([...selectedUserSet, user.email]));
+    setSearchTerm('');
+    setSuggestions([]);
+    inputRef.current.focus();
+  }
+
+  const handleRemoveUser = (user) => {
+    const updatedUsers = selectedUsers.filter((selectedUser) => selectedUser.id !== user.id);
+    setSelectedUsers(updatedUsers);
+
+    const updatedEmails = new Set(selectedUserSet);
+    updatedEmails.delete(user.email);
+    setSelectedUserSet(updatedEmails);
+  }
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Backspace' && e.target.value === '' && selectedUsers.length > 0) {
+      const lastUser = selectedUsers[selectedUsers.length - 1];
+      handleRemoveUser(lastUser);
+      setSuggestions([]);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Multi Select Search Using React</h1>
+      {selectedUsers.map((user) => {
+        return <Pill key={user.email}
+        image={user.image}
+        text={`${user.firstName} ${user.lastName}`}
+        onClick={() => handleRemoveUser(user)} />
+      })}
+      <div className='user-search-input'>
+        <input ref={inputRef} placeholder='Enter Your Search' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} onKeyDown={handleKeyDown}/>
+        <ul className='suggestions-list'>
+          {suggestions?.users?.map((user) => {
+            return !selectedUserSet.has(user.email) ? (<li key={user.email} onClick={() => handleSelectUser(user)}>
+              <img src={user.image} alt={`${user.firstName}`} />
+              <span>{`${user.firstName} ${user.lastName}`}</span>
+            </li>) : <></>
+})}
+        </ul>
+      </div>
     </div>
   );
 }
